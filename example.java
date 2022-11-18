bd84dc1b1360914a12b41cbab044a42cc56c86d6
Ganesh A K <78320451+ganeshak4443@users.noreply.github.com>
2022-11-18 12:46:02 +0530

    Add files via upload

--- example.java
+++ example.java
@@ -1,5 +1,5 @@
-public class example{
-    public static void Main(string []args){
-        System.println("hi ganesh");
+public class example {
+    public static void main(String []args){
+        System.out.println("hi ganesh");
     }
 }
